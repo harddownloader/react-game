399e6c7210b8b456c11d1aaa5bb8f1a1e51cc7cb
harddownloader <serafimdev@gmail.com>
2021-06-12 13:57:13 +0300

    SSR - add SEO tags

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import App from '../src/App.js';
+import Head from 'next/head'
 
 function HomePage() {
-  return <><App/></>
+  return (
+    <>
+      <Head>
+        <title>Spaceships Game</title>
+        <meta name="keywords" content="React Spaceships Game" />
+        <meta name="description" content="React Spaceships Game" />
+        <meta charSet="utf-8" />
+      </Head>
+      <App/>
+    </>
+  )
 }
 
 export default HomePage
